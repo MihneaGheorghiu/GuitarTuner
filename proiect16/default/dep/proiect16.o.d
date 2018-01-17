03a399b925aef900753c869fe2ea8ce0a1d0495a
Mihnea Gheorghiu <MihneaGheorghiu@users.noreply.github.com>
2018-01-18 00:14:46 +0200

    Add files via upload

---
+++ proiect16/default/dep/proiect16.o.d
@@ -0,0 +1,37 @@
+proiect16.o: ../proiect16.c \
+  c:/winavr-20071221/bin/../avr/include/avr/io.h \
+  c:/winavr-20071221/bin/../avr/include/avr/sfr_defs.h \
+  c:/winavr-20071221/bin/../avr/include/inttypes.h \
+  c:/winavr-20071221/bin/../avr/include/stdint.h \
+  c:/winavr-20071221/bin/../avr/include/avr/iom16.h \
+  c:/winavr-20071221/bin/../avr/include/avr/portpins.h \
+  c:/winavr-20071221/bin/../avr/include/avr/common.h \
+  c:/winavr-20071221/bin/../avr/include/avr/version.h \
+  c:/winavr-20071221/bin/../avr/include/avr/fuse.h \
+  c:/winavr-20071221/bin/../avr/include/avr/lock.h \
+  c:/winavr-20071221/bin/../avr/include/avr/interrupt.h \
+  c:/winavr-20071221/bin/../avr/include/compat/deprecated.h
+
+c:/winavr-20071221/bin/../avr/include/avr/io.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20071221/bin/../avr/include/inttypes.h:
+
+c:/winavr-20071221/bin/../avr/include/stdint.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/iom16.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/portpins.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/common.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/version.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/fuse.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/lock.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/interrupt.h:
+
+c:/winavr-20071221/bin/../avr/include/compat/deprecated.h:
